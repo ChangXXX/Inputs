7018cf315a9dab377360b6fe3bee67e2ac647d03
changXXX <53431177+ChangXXX@users.noreply.github.com>
2024-10-31 03:15:17 +0900

    add theme

--- core/designsystem/src/main/java/git/changxxx/core/designsystem/theme/Color.kt
+++ core/designsystem/src/main/java/git/changxxx/core/designsystem/theme/Color.kt
@@ -2,10 +2,24 @@ package git.changxxx.core.designsystem.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Light Theme Colors
+val LightPrimary = Color(0xFFF1F0E8)
+val LightOnPrimary = Color(0xFF000000)
+val LightPrimaryContainer = Color(0xFFC7C3B8)
+val LightSecondary = Color(0xFF3A3A3A)
+val LightOnSecondary = Color(0xFFFFFFFF)
+val LightBackground = Color(0xFFFFFFFF)
+val LightOnBackground = Color(0xFF000000)
+val LightSurface = Color(0xFFEAEAEA)
+val LightOnSurface = Color(0xFF000000)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Dark Theme Colors
+val DarkPrimary = Color(0xFFF1F0E8)
+val DarkOnPrimary = Color(0xFF000000)
+val DarkPrimaryContainer = Color(0xFFC7C3B8)
+val DarkSecondary = Color(0xFFE0E0E0)
+val DarkOnSecondary = Color(0xFF000000)
+val DarkBackground = Color(0xFF121212)
+val DarkOnBackground = Color(0xFFFFFFFF)
+val DarkSurface = Color(0xFF1E1E1E)
+val DarkOnSurface = Color(0xFFFFFFFF)
--- feature/note/src/main/java/git/changxxx/feature/note/NoteList.kt
+++ feature/note/src/main/java/git/changxxx/feature/note/NoteList.kt
@@ -3,9 +3,7 @@ package git.changxxx.feature.note
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
-import git.changxxx.core.designsystem.theme.InputsTheme
 import git.changxxx.core.navigation.Route
 
 @Composable
